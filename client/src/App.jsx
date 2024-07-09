e7f8559af49a130a302f9fdb64220a4340db2baa
hamdanzbr <hamdanzbr46@gmail.com>
2024-07-09 23:12:43 +0530

    create the header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,10 +6,12 @@ import Signup from './pages/Signup'
 import Signnin from './pages/Signnin'
 import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
+import Header from './components/Header'
 
 function App() {
   return (
    <BrowserRouter>
+   <Header/>
    <Routes>
     <Route path='/' element={<Home />} ></Route>
     <Route path='/about' element={<About/>} ></Route>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,17 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+// tailwind.config.js
+module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    'node_modules/daisyui/dist/**/*.js', // Add this line to include Daisy UI's files
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: ["light"], // Set light as the first theme, making it the default
+  },
 }
